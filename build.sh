abc7c31582c727863f24aef08cda58ace7839794
Alekxays <admin@alexandreboissel.me>
2025-02-22 15:06:26 +0100

    fix : CD removing

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Installation des dépendances système nécessaires
-apt-get update -y
-apt-get install -y python build-essential libtool-bin
-
-# Installation des dépendances npm
-npm install
-npm rebuild sodium-native
